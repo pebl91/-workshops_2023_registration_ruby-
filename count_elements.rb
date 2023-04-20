135ae048ad5611e3f4cd667ca16f168cacb96f05
Paweł Smusz <pawel_smusz@yahoo.com>
2023-04-20 17:50:16 +0200

    my solutions

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,14 @@
 class CountElements
-
-end
+    def initialize(table)
+      @table = table
+    end
+  
+    def perform
+      count_hash = Hash.new(0)
+      @table.each do |element|
+        count_hash[element] += 1
+      end
+      count_hash
+    end
+  end
+  
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,19 @@
 class ValidatePassword
-
-end
+    attr_reader :pass
+    
+    def initialize(pass)
+      @pass = pass
+    end
+    
+    def perform
+      return false if pass.length < 6 || pass.length > 24
+      return false unless pass =~ /[A-Z]/
+      return false unless pass =~ /[a-z]/
+      return false unless pass =~ /\d/
+      return false unless pass =~ /[!@#$%&*+=:;?<>\[\]{}()]/
+      return false if pass.chars.each_cons(3).any? { |a, b, c| a == b && b == c }
+      
+      true
+    end
+  end
+  
